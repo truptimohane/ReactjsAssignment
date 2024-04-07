ec47594c9b1b55aaf29ff83da870ccca149d9f0d
truptimohane <truptimohane48@gmail.com>
2024-04-07 18:09:05 +0530

    changes

--- ReactRedux/crud/src/App.js
+++ ReactRedux/crud/src/App.js
@@ -1,7 +1,39 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./features/pages/Login";
+import Register from "./features/pages/Register";
+import Dashboard from "./features/pages/Dashboard";
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Login />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/register"
+            element={
+              <>
+                <Register />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/dashboard"
+            element={
+              <>
+                <Dashboard />
+              </>
+            }
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
